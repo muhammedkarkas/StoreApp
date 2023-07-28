8bfbe68b8fd93d27993ec9287ce387c80a946307
muhammed.karkas <muhammed.karkas@gmail.com>
2023-07-28 06:15:03 +0300

    .

--- StoreApp/Controllers/ProductController.cs
+++ StoreApp/Controllers/ProductController.cs
@@ -6,14 +6,21 @@ namespace StoreApp.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Product> Index()
         {
-            var context = new RepositoryContext(
-                new DbContextOptionsBuilder<RepositoryContext>()
-                .UseSqlite("Data Source = C:\\MVC\\ProductDb.db")
-                .Options
-                );
-            return context.Products;
+            //var context = new RepositoryContext(
+            //    new DbContextOptionsBuilder<RepositoryContext>()
+            //    .UseSqlite("Data Source = C:\\MVC\\ProductDb.db")
+            //    .Options
+            //    );
+            return _context.Products;
         }
     }
 }
