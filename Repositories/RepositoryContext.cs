2bc80c2af0314ac683a68d2d5189315cb0ed04e6
muhammed.karkas <muhammed.karkas@gmail.com>
2023-08-03 03:58:33 +0300

    .

---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product() { ProductId = 1, ProductName = "Computer", Price = 17000 },
+                    new Product() { ProductId = 2, ProductName = "Notebook", Price = 17000 },
+                    new Product() { ProductId = 3, ProductName = "Phone", Price = 17000 },
+                    new Product() { ProductId = 4, ProductName = "Tablet", Price = 17000 },
+                    new Product() { ProductId = 5, ProductName = "İpad", Price = 17000 }
+                );
+        }
+    }
+}
