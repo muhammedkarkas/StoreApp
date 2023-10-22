89cfcc0b30d445609d59dc2765e4ff44f92debd4
muhammed.karkas <muhammed.karkas@gmail.com>
2023-10-23 02:45:08 +0300

    .

---
+++ StoreApp/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(model.Name);
+
+                if(user is not null)
+                {
+                    //Oturum açma işlemleri
+                    await _signInManager.SignOutAsync(); //açık oturum varsa sonlandırıldı
+
+                    if((await _signInManager.PasswordSignInAsync(user, model.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(model?.ReturnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError("Error", "Invalid username or password.");
+            }
+            return View();
+        }
+    }
+}
