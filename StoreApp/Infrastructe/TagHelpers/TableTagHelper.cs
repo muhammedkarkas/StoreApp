16a8d94d86f119de2971ff60adc59e2b9239cef0
muhammed.karkas <muhammed.karkas@gmail.com>
2023-10-22 21:45:04 +0300

    .

---
+++ StoreApp/Infrastructe/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    [HtmlTargetElement("table")]
+    public class TableTagHelper : TagHelper
+    {
+        //Bu method ile Html etiketleri manipule edilebilmektedir.
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //Sayfa içerisinde tanımlanan tablelarda otomatik olarak bootstrapın table özelliğinin eklenmesi sağlanmaktadır.
+            output.Attributes.SetAttribute("class", "table table-hover");
+        }
+    }
+}
