dc448a5d4cdc2e42ddcf9ba71f1f1376ac4d90ba
muhammed.karkas <muhammed.karkas@gmail.com>
2023-10-24 00:32:05 +0300

    .

---
+++ Entities/Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "UserName is required.")]
+        public String? UserName { get; init; }
+        [Required(ErrorMessage = "Email is required.")]
+        public String? Email {  get; init; }
+        [Required(ErrorMessage = "Password is required.")]
+        public String? Password { get; init; }
+    }
+}
--- StoreApp/Controllers/AccountController.cs
+++ StoreApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Entities.Dtos;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StoreApp.Models;
 
@@ -49,6 +50,37 @@ namespace StoreApp.Controllers
         {
             await _signInManager.SignOutAsync();
             return Redirect(returnUrl);
+        }
+
+        public async Task<IActionResult> Register([FromForm] RegisterDto model)
+        {
+            //Kullanıcı kaydı
+            var user = new IdentityUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            //kullanıcı bilgilerinin database'e aktarılması
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if(result.Succeeded)
+            {
+                //Kullanıcı role tanımı
+                var roleResult = await _userManager.AddToRoleAsync(user, "User");
+
+                if (roleResult.Succeeded)
+                    return RedirectToAction("Login", new { ReturnUrl = "/"});
+            }
+            else
+            {   
+                //User eklenemezse eğer hatalara bakılacaktır. Modelstate içerisine eklenecektir.
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+
+            return View();
         }
     }
 }
